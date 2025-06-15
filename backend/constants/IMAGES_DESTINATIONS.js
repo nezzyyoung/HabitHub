ae90bfb3efc971ffe2c8614565fcf31961d4764d
nezzyyoung <nezzybucumi@gmail.com>
2025-06-16 00:39:12 +0300

    updates

---
+++ backend/constants/IMAGES_DESTINATIONS.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const IMAGES_DESTINATIONS = {
+          publicite:path.sep +'uploads' + path.sep + 'images' + path.sep + 'publicite',
+}
+
+module.exports = IMAGES_DESTINATIONS
