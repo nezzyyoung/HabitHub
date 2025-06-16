90e613d0327d3bfa82f53a4bf299efd5671d8886
nezzyyoung <nezzybucumi@gmail.com>
2025-06-16 13:52:03 +0300

    updates

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,42 +1,19 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../utils/sequerize');
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../utils/sequerize");
 
-const Users = sequelize.define('Users', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
+
+const Users = sequelize.define(
+  "Users", {
   name: {
     type: DataTypes.STRING,
-    allowNull: false,
   },
   email: {
     type: DataTypes.STRING,
     unique: true,
-    allowNull: false,
   },
-  password_hash: {
+  password: {
     type: DataTypes.STRING,
-    allowNull: false,
   },
-  role: {
-    type: DataTypes.ENUM('admin', 'teacher', 'student'),
-    allowNull: false,
-  },
-  profile_picture_url: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  status: {
-    type: DataTypes.ENUM('active', 'inactive'),
-    allowNull: false,
-    defaultValue: 'active',
-  },
-}, {
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
 });
 
 module.exports = Users;
