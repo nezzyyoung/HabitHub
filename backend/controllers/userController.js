83293c9da940ec5563c0773a43ddecbb07ce0a2b
nezzyyoung <nezzybucumi@gmail.com>
2025-06-20 18:20:37 +0300

    updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
   try {
@@ -15,5 +16,23 @@ exports.getUser = async (req, res) => {
     res.json(user);
   } catch (err) {
     res.status(404).json({ message: 'User not found' });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const isValidPassword = await user.validatePassword(password);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET || require('crypto').randomBytes(64).toString('hex'), { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: 'Error logging in' });
   }
 };
