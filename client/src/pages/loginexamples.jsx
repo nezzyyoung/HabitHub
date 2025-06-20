e5e77f9f2adcd26462de06c2f11cd8c08a1f8139
nezzyyoung <nezzybucumi@gmail.com>
2025-06-20 18:21:06 +0300

    updates

---
+++ client/src/pages/loginexamples.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const Loginexamples = () => {
+    return (
+        <View>
+            <Text style={styles.login}>Hello World</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    login: {
+        marginBottom: 20,
+    }
+})
+
+export default Loginexamples;
