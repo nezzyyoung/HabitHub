b6767f98bf5771254b7433ebff2f9655802a5ee0
nezzyyoung <nezzybucumi@gmail.com>
2025-06-25 09:36:36 +0300

    updated json format for log in and sign up

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../utils/sequerize");
-
+const bcrypt = require('bcrypt');
 
 const Users = sequelize.define(
   "Users", {
@@ -14,6 +14,26 @@ const Users = sequelize.define(
   password: {
     type: DataTypes.STRING,
   },
+}, {
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
 });
+
+// Instance method to validate password
+Users.prototype.validatePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = Users;
